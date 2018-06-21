793416d01a169d0df9866a03281192ef0a5272d8
rina.fukamatsu <rina.fukamatsu@rakuten.com>
2018-06-21 00:45:02 +0000

    user registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/'; //homeを消した
 
     /**
      * Create a new controller instance.
