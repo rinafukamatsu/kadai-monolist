ade6a89ccdce1b6a0fd098a0cd5b54b75c87c708
rina.fukamatsu <rina.fukamatsu@rakuten.com>
2018-06-22 00:53:43 +0000

    ranking

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -1,6 +1,6 @@
 @if ($items)
     <div class="row">
-        @foreach ($items as $item)
+        @foreach ($items as $key => $item)
             <div class="item">
                 <div class="col-md-3 col-sm-4 col-xs-12">
                     <div class="panel panel-default">
@@ -16,10 +16,16 @@
                             <div class="buttons text-center">
                                 @if (Auth::check())
                                     @include('items.want_button', ['item' => $item])
-                                     @include('items.have_button', ['item' => $item]) <!-- ここかえたよ -->
+                                    @include('items.have_button', ['item' => $item])
                                 @endif
                             </div>
                         </div>
+                        @if (isset($item->count))
+                            <div class="panel-footer">
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Wants</p>
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Has</p>  <!-- ここかえたよ -->
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
